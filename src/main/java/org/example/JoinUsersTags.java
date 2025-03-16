a3d94577a4e059862f31dd011302ac8efd4177d1
IwoSzczepniak <iszczepaniak@student.agh.edu.pl>
2025-03-16 11:06:11 +0100

    4.8 done

---
+++ src/main/java/org/example/JoinUsersTags.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.apache.spark.sql.*;
+import static org.apache.spark.sql.functions.*;
+import java.util.List;
+
+public class JoinUsersTags {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("JoinUsersTags")
+                .master("local")
+                .getOrCreate();
+        System.out.println("Using Apache Spark v" + spark.version());
+
+        Dataset<Row> df_users = spark.read()
+                .format("csv")
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .load("src/main/resources/users.csv");
+
+        Dataset<Row> df_tags = spark.read()
+                .format("csv")
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .load("src/main/resources/tags.csv");
+
+        df_users.createOrReplaceTempView("users");
+        df_tags.createOrReplaceTempView("tags");
+
+        String query = """
+                SELECT u.email, t.tag
+                FROM users u
+                JOIN tags t ON u.userId = t.userId
+                """;
+
+        Dataset<Row> df_ut = spark.sql(query);
+
+        Dataset<Row> df_grouped = df_ut.groupBy("email")
+                .agg(concat_ws(" ", collect_list("tag")).alias("tags"));
+
+        System.out.println("Zgrupowane tagi po użytkownikach:");
+        df_grouped.show();
+
+        System.out.println("\nLista tagów dla każdego użytkownika:");
+        List<Row> tagsList = df_grouped.select("tags").collectAsList();
+        // for (Row row : tagsList) {
+        //     System.out.println(row.getString(0));
+        // }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        JoinMoviesRatingsGenres.main(null);
+        JoinUsersTags.main(null);
     }
 }
